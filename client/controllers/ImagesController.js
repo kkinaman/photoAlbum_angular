129e491188dc7e84a4dc4112e9d56e2d67a158c9
Karina Kinaman <kkinaman@alumni.virginia.edu>
2016-10-16 14:29:15 -0700

    Add dropdown to allow user to change rating of photo
    Selecting from dropdown will also reorder the list of images and rerender the rating displayed below the featured image

--- client/controllers/ImagesController.js
+++ client/controllers/ImagesController.js
@@ -3,9 +3,34 @@ angular.module('photoAlbum.images', [])
 .controller('ImagesController', function($scope) {
   $scope.images = imagesData;
   $scope.featImage = {};
+  $scope.addImageTitle = '';
+  $scope.addImageUrl = '';
+
   $scope.display = function(image) {
     $('.featImageDisplay').text('');
     $scope.featImage = image;
     $('.featImageRating').text($scope.featImage.rating + ' out of 5');
+    $('.dropdown').removeClass('hidden');
   };
+
+  $scope.addNewImage = function() {
+    // $scope.images.push({
+    //   url: $scope.addImageUrl,
+    //   name: $scope.addImageTitle,
+    //   rating: 0
+    // });
+    imagesData.push({
+      url: $scope.addImageUrl,
+      name: $scope.addImageTitle,
+      rating: 0
+    });
+    $scope.addImageTitle = '';
+    $scope.addImageUrl = '';
+  };
+
+  $scope.changeRating = function(rating) {
+    console.log('changing rating to', rating);
+    $scope.featImage.rating = rating;
+    $('.featImageRating').text($scope.featImage.rating + ' out of 5');
+  }
 });
